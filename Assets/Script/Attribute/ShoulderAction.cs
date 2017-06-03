27b49950cd9ed17b7ef0c93c7c7f29239d10131c
park <wlgur01014@naver.com>
2017-06-03 19:36:23 +0900

    enemy ai start

--- Assets/Script/Attribute/ShoulderAction.cs
+++ Assets/Script/Attribute/ShoulderAction.cs
@@ -9,34 +9,33 @@ public class ShoulderAction : MonoBehaviour {
 
 	private Animator parentAnimator;
 	private Animator shoulderAnimator;
-
-	// Use this for initialization
+    
 	void Start ()
 	{
 		parentAnimator = transform.parent.GetComponent<Animator> ();
 		shoulderAnimator = GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (!parentAnimator.GetBool ("HoldOnWeapon")) {
-			return;
-		}
+    
+    void Update()
+    {
+        if (!parentAnimator.GetBool("HoldOnWeapon"))
+        {
+            return;
+        }
 
-		isShooting = Input.GetMouseButton (0);
-		isAimming = Input.GetMouseButton (1);
+        isShooting = Input.GetMouseButton(0);
+        isAimming = Input.GetMouseButton(1);
 
-		parentAnimator.SetBool ("IsShooting", isShooting);
-		shoulderAnimator.SetBool ("IsShooting", isShooting);
+        parentAnimator.SetBool("IsShooting", isShooting);
+        shoulderAnimator.SetBool("IsShooting", isShooting);
 
-		parentAnimator.SetBool ("IsAimming", isAimming);
-		shoulderAnimator.SetBool ("IsAimming", isAimming);
+        parentAnimator.SetBool("IsAimming", isAimming);
+        shoulderAnimator.SetBool("IsAimming", isAimming);
 
-		if (isShooting)
-		{
-			parentAnimator.SetTrigger ("TriggerShot");
-			shoulderAnimator.SetTrigger ("TriggerShot");
-		}
-	}
+        if (isShooting)
+        {
+            parentAnimator.SetTrigger("TriggerShot");
+            shoulderAnimator.SetTrigger("TriggerShot");
+        }
+    }
 }
