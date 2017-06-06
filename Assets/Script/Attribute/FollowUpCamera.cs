1ce9bcc098f6521f5c17a72433271897d968a7df
= <jsm1505104@gmail.com>
2017-06-07 05:05:25 +0900

    Bug FIx

---
+++ Assets/Script/Attribute/FollowUpCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUpCamera : MonoBehaviour {
+
+	public Transform followTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (followTarget.position.x,followTarget.position.y , transform.position.z);
+	}
+}
