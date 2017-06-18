f7e41d743c00bf4e3feec62bd90cf80467ab4a5c
SungMin <jsm1505104@gmail.com>
2017-06-19 02:09:54 +0900

    Bubble Dialogue Added

--- Assets/Script/Architecture/NPCAction.cs
+++ Assets/Script/Architecture/NPCAction.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class NPCAction : MonoBehaviour {
 
 	public int tryCountForActivate;
 	public int maxActionCircle = 1;
 	protected int actionCirlce = 0;
+
+	public UnityEvent finishEvent;
 
 	protected bool IsActionFinished {
 		get;
@@ -17,4 +20,5 @@ public abstract class NPCAction : MonoBehaviour {
 	protected abstract bool IsSatisfied (ref int tryCount);
 	protected abstract void DoAction ();
 	public abstract void CancelAction ();
+	protected abstract void FinishedAction();
 }
