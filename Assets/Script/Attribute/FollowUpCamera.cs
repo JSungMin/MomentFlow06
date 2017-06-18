37d3307387be9e3ecc976d9e6e3b977fcf4ba6c2
SungMin <jsm1505104@gmail.com>
2017-06-19 03:02:24 +0900

    Pro Cam Added

--- Assets/Script/Attribute/FollowUpCamera.cs
+++ Assets/Script/Attribute/FollowUpCamera.cs
@@ -6,13 +6,23 @@ public class FollowUpCamera : MonoBehaviour {
 
 	public Transform followTarget;
 
+	public Vector2 noneFollowOffset;
+
+	private float minWidth, minHeight;
+	private float maxWidth, maxHeight;
+
+	private Vector2 deltaPos;
+
 	// Use this for initialization
 	void Start () {
-		
+		minWidth = -noneFollowOffset.x;
+		maxWidth = noneFollowOffset.x;
+		minHeight = -noneFollowOffset.y;
+		maxHeight = noneFollowOffset.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (followTarget.position.x,followTarget.position.y , transform.position.z);
+		deltaPos = new Vector2 (transform.position.x - followTarget.position.x, transform.position.y - followTarget.position.y);
 	}
 }
